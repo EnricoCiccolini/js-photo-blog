53226e39a2edd42ab739cc23ac93deba4446f319
EnricoCiccolini <piroroma90@gmail.com>
2025-03-17 12:32:30 +0100

    feat: add modul

--- main.js
+++ main.js
@@ -5,17 +5,29 @@ const urlClient = 'https://lanciweb.github.io/demo/api/pictures/'
 
 const wallElement = document.getElementById('container-photo')
 
+const overElement = document.getElementById('over')
+const replaceElement = document.getElementById('replace')
 
 let result = ''
+
+
+const buttonElement = document.querySelector('.button')
+
+buttonElement.addEventListener('click', function () {
+    overElement.classList.remove('d-block')
+    overElement.classList.add('d-none')
+
+})
+
 axios.get(urlClient).then((response) => {
     // console.log(response.data['0'].url )
     const ceck = response.data
     for (let i = 0; i < ceck.length; i++) {
 
-       // console.log(' sono nel ciclo')
-       // console.log(ceck[i])
+        // console.log(' sono nel ciclo')
+        // console.log(ceck[i])
         result = result + `
-    <div class="col-12 col-md-6 col-lg-4 col-xl-4" style="width: 18rem;">
+    <div id="${i}" class="col-12 col-md-6 col-lg-4 col-xl-4" style="width: 18rem;">
         <div class="card " style="width: 18rem;">
             <div class='p-4 position-static bg-light'>
                 <div class="position-absolute top-0 start-50 translate-middle"><img src="img/pin.svg"
@@ -35,6 +47,19 @@ axios.get(urlClient).then((response) => {
 
         wallElement.innerHTML = result
     }
+    const cardElement = document.querySelectorAll('.card')
+    console.log(cardElement)
+    for (let i = 0; i < cardElement.length; i++) {
+        cardElement[i].addEventListener("click", function () {
+            const currentcard = this.querySelector('.card-img-top')
+
+            replaceElement.src = currentcard.src
+            overElement.classList.remove('d-none')
+            overElement.classList.add('d-block')
+
+        })
+    }
+
 })
     .catch(error => {
         console.log("errore ")
